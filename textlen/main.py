edf08e013627b057c2fcc37e6b138f399e886981
Rud Faden <rudfaden@gmail.com>
2025-11-09 19:54:43 +0100

    refactor: simplify textlen function by removing unused imports and enhancing readability

--- textlen/main.py
+++ textlen/main.py
@@ -1,26 +1,18 @@
 import typer
-from rich.console import Console
-from rich.table import Table
 
 app = typer.Typer()
-console = Console()
 
 
 @app.command()
-def textlen(string: str, trim: bool = typer.Option(
-        False, help="Trim leading and trailing whitespace")) -> None:
-    """Returns the length of a string
-
-    Args:
-        string (str): any string
-        trim (bool): whether to trim whitespace
-    """
+def textlen(
+    string: str,
+    trim: bool = typer.Option(False, help="Trim leading and trailing whitespace"),
+) -> None:
+    """Returns the length of a string"""
     if trim:
         string = string.strip()
-    length = str(len(string))
-    table = Table("String", "Length")
-    table.add_row(string, length)
-    console.print(table)
+    typer.echo(f"String: {string}")
+    typer.secho(f"Length: {len(string)}", fg=typer.colors.GREEN)
 
 
 if __name__ == "__main__":
